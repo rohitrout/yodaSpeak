9059bd5cf46315a6670c57f72d312283622521e1
Rohit Rout <rohitrout@outlook.com>
2021-09-30 08:31:33 +0530

    code updated for yoda

---
+++ app.js
@@ -0,0 +1,33 @@
+var txtInput=document.querySelector("#txt-area")
+var txtOutput=document.querySelector("#output")
+var btnTranslate=document.querySelector("#btn-translate")
+
+var serverURL="https://api.funtranslations.com/translate/yoda.json"
+
+btnTranslate.addEventListener("click",clickEventHandler)
+
+function constructURL(text)
+{
+    return serverURL + "?"+"text=" + text;
+
+}
+
+function doFetch(text)
+{
+    
+}
+
+function clickEventHandler()
+{
+    var textInput= txtInput.value;
+
+    fetch(constructURL(textInput))
+    .then (response => response.json())
+    .then (json =>{
+        var translatedText=json.contents.translated;
+        txtOutput.innerText=translatedText;
+    })
+}
+
+
+
